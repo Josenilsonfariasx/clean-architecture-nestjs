5127af7080594703cf6471489f997a8959778789
Farias4 <farias@reobote.tec.br>
2025-01-26 04:10:36 -0300

    feat: Define UserType and update UserEntity with type-based constructor

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,7 @@
-export class UserEntity {}
+import { UserType } from '@/types/user/user.type';
+
+export class UserEntity {
+  constructor(public readonly props: UserType) {
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+}
