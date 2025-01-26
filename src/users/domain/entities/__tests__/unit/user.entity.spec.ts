f36b877ca64c48e0759017cbce6dae975c529237
Farias4 <farias@reobote.tec.br>
2025-01-26 04:23:05 -0300

    test: Add unit tests for UserEntity with Faker data generation

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,21 @@
+import { UserType } from '@/types/user/user.type';
+import { UserEntity } from '../../user.entity';
+import { faker } from '@faker-js/faker';
+describe('UserEntity unit tests', () => {
+  let props: UserType;
+  let sut: UserEntity;
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+    sut = new UserEntity(props);
+  });
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
