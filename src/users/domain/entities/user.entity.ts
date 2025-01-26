99f9905493d5dda68ffa182ef07acc01e0fe01af
Farias4 <farias@reobote.tec.br>
2025-01-26 14:03:06 -0300

    feat: Add getter methods to UserEntity for accessing user properties

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -4,4 +4,19 @@ export class UserEntity {
   constructor(public readonly props: UserType) {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
+  get name() {
+    return this.props.name;
+  }
+
+  get email() {
+    return this.props.email;
+  }
+
+  get password() {
+    return this.props.password;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
 }
